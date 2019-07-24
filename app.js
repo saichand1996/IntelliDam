7312c4de9d92c970a260d45c06d2d0b93c0c5029
vmatti <vmatti@DIN13002731.corp.capgemini.com>
2019-07-24 16:56:40 +0530

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+
+/**
+ * Generated Node.js application that can run on IBM Bluemix
+ */
+
+var http = require("http"),
+	fs = require("fs");
+var express = require("express");
+var app = express();
+
+var appport = process.env.PORT || 8888;
+
+app.use(express.static(__dirname + '/view'));
+
+app.get('*', function(req, res) {
+	res.sendFile('index.html',{ root: 'view' });
+});
+app.listen(appport);
+
+
