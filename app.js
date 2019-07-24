be635de16961a5739d8c4e38d512f0caa668578a
chandu <saichand1102516@gmail.com>
2019-07-25 01:34:21 +0530

    rendering undderstood

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ var app = express();
 var appport = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/view'));
+app.use(express.json({ limit : '1mb'}));
 
 app.get('*', function(req, res) {
-	res.sendFile('index.html',{ root: 'view' });
+	res.sendFile('index.html',{ root: 'view' });	
 });
+
+app.get('/init',function(req, res){
+	res.json({ 
+		damName : 'idukki'
+		waterLevel : '900' 
+	})
+});	
 app.listen(appport);
