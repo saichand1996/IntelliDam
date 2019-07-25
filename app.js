84bf716ceac2ca451ec801b55c701f3eb2af72ba
vmatti <vmatti@DIN13002731.corp.capgemini.com>
2019-07-25 20:26:54 +0530

    rendering enabled and webscraping acheived

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
 var http = require("http"),
 	fs = require("fs");
 var express = require("express");
+const request = require('request');
+const cheerio = require('cheerio');
+const tabletojson = require('tabletojson');
+
 var app = express();
 
 var appport = process.env.PORT || 8888;
@@ -13,14 +17,22 @@ var appport = process.env.PORT || 8888;
 app.use(express.static(__dirname + '/view'));
 app.use(express.json({ limit : '1mb'}));
 
-app.get('*', function(req, res) {
+
+app.get('/api',function(req, res) {
+
+	tabletojson.convertUrl(
+		'https://irrigationap.cgg.gov.in/wrd/dashBoard',
+		{ stripHtmlFromCells: true },
+		function(tablesAsJson) {
+		  //Print out the 1st row from the 2nd table on the above webpage as JSON 
+		   var data = (tablesAsJson[1][23]);
+		   console.log(data);
+		  res.json(JSON.parse(JSON.stringify(data)));
+		}
+	  );
+
+});	
+app.get('*', function(req, res) {	
 	res.sendFile('index.html',{ root: 'view' });	
 });
-
-app.get('/init',function(req, res){
-	res.json({ 
-		damName : 'idukki'
-		waterLevel : '900' 
-	})
-});	
 app.listen(appport);
