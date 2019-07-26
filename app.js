836f638f93665a88c017ae8c7f923e61828983b9
vmatti <vmatti@DIN13002731.corp.capgemini.com>
2019-07-26 12:38:38 +0530

    2 dams data rendering enabled

--- app.js
+++ app.js
@@ -18,20 +18,46 @@ app.use(express.static(__dirname + '/view'));
 app.use(express.json({ limit : '1mb'}));
 
 
-app.get('/api',function(req, res) {
-
+app.get('/dam1',function(req, res) {
 	tabletojson.convertUrl(
 		'https://irrigationap.cgg.gov.in/wrd/dashBoard',
 		{ stripHtmlFromCells: true },
 		function(tablesAsJson) {
-		  //Print out the 1st row from the 2nd table on the above webpage as JSON 
-		   var data = (tablesAsJson[1][23]);
-		   console.log(data);
+		  //Print out the 1st row from the 2nd table on the above webpage as JSON		   
+		   //console.log(data);
+		   for(i=1;i<40;i++){
+				var data = (tablesAsJson[1][i]);
+				obj = JSON.parse(JSON.stringify(data));
+				if(obj["Reservoir\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tName_2"]=="SRISAILAM"){
+					console.log(data);
+					break;
+				}
+		   }
 		  res.json(JSON.parse(JSON.stringify(data)));
 		}
 	  );
-
 });	
+
+app.get('/dam2',function(req, res) {
+	tabletojson.convertUrl(
+		'https://irrigationap.cgg.gov.in/wrd/dashBoard',
+		{ stripHtmlFromCells: true },
+		function(tablesAsJson) {
+		  //Print out the 1st row from the 2nd table on the above webpage as JSON		   
+		   //console.log(data);
+		   for(i=1;i<40;i++){
+				var data = (tablesAsJson[1][i]);
+				obj = JSON.parse(JSON.stringify(data));
+				if(obj["Reservoir\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tName_2"]=="NAGARJUNA SAGAR"){
+					console.log(data);
+					break;
+				}
+		   }
+		  res.json(JSON.parse(JSON.stringify(data)));
+		}
+	  );
+});	
+
 app.get('*', function(req, res) {	
 	res.sendFile('index.html',{ root: 'view' });	
 });
