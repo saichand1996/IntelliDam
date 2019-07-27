80cd97edab14ef545507a7e2f8107f3397d75834
vmatti <vmatti@DIN13002731.corp.capgemini.com>
2019-07-27 20:36:17 +0530

    integrated with COO

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ var express = require("express");
 const request = require('request');
 const cheerio = require('cheerio');
 const tabletojson = require('tabletojson');
+const AWS  = require('ibm-cos-sdk');
+
+const config = {
+    endpoint: 's3.eu.cloud-object-storage.appdomain.cloud',
+    apiKeyId: 'OkzCQExhDQy8Pk9TzMbTEDddM4LQdWgWcv-ZH7ENafvC',
+    serviceInstanceId: 'crn:v1:bluemix:public:cloud-object-storage:global:a/9b204a210243411b914dde8c0c4302b1:7c0caf31-789e-421c-87a4-d16a8b1c0948::',
+};
+
+var cos = new AWS.S3(config);
+
+function getItem(bucketName, itemName, callback) {
+    console.log(`Retrieving item from bucket: ${bucketName}, key: ${itemName}`);
+    return cos.getObject({
+        Bucket: bucketName, 
+        Key: itemName
+    }).promise()
+    .then((data) => {
+        if (data != null) {
+			//console.log('File Contents: ' + Buffer.from(data.Body).toString());
+			callback(Buffer.from(data.Body).toString());
+        }    
+    })
+    .catch((e) => {
+        console.error(`ERROR: ${e.code} - ${e.message}\n`);
+    });
+}
 
 var app = express();
 
@@ -29,14 +55,25 @@ app.get('/dam1',function(req, res) {
 				var data = (tablesAsJson[1][i]);
 				obj = JSON.parse(JSON.stringify(data));
 				if(obj["Reservoir\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tName_2"]=="SRISAILAM"){
-					console.log(data);
+					//console.log(data);
 					break;
 				}
 		   }
 		  res.json(JSON.parse(JSON.stringify(data)));
 		}
 	  );
+	  getItem('rainfall-donotdelete-pr-jdznbtggz5fj5t','Estimation.csv', function(result){		
+		jsonData  = JSON.parse(JSON.stringify(data));
+		console.log(jsonData);
+	});	 
 });	
+
+app.get('/predict1',function(req,res){
+	  getItem('rainfall-donotdelete-pr-jdznbtggz5fj5t','Estimation.csv', function(result){
+		console.log(result);
+		JSON.parse(JSON.stringify(data))
+	});
+});
 
 app.get('/dam2',function(req, res) {
 	tabletojson.convertUrl(
@@ -49,7 +86,7 @@ app.get('/dam2',function(req, res) {
 				var data = (tablesAsJson[1][i]);
 				obj = JSON.parse(JSON.stringify(data));
 				if(obj["Reservoir\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tName_2"]=="NAGARJUNA SAGAR"){
-					console.log(data);
+					//console.log(data);
 					break;
 				}
 		   }
